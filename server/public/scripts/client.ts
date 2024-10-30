38af23b9a02c127a78f6161536e1decd26451869
Jacob Larson <hockeybobber@comcast.net>
2024-10-29 22:20:00 -0500

    added get route for all of the todos

--- server/public/scripts/client.ts
+++ server/public/scripts/client.ts
@@ -1,1 +1,14 @@
 console.log('JS is sourced!');
+
+
+//get route
+function getTodos() {
+    console.log('in get route');
+
+  axios({
+    url: '/todos',
+    method: 'GET'
+  }).then((response) => {
+    renderTodos(response.data)
+  })
+}
