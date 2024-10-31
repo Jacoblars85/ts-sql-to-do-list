7e6ff8cb719c625f732215240e6b7656647e2ccb
Jacob Larson <hockeybobber@comcast.net>
2024-10-30 21:16:43 -0500

    added a render function

--- server/public/scripts/client.ts
+++ server/public/scripts/client.ts
@@ -12,3 +12,29 @@ function getTodos() {
     renderTodos(response.data)
   })
 }
+
+//render route
+function renderTodos(todos) {
+    console.log('in render function');
+    const toDoBody = document.getElementById('todoBody');
+    toDoBody.innerHTML = '';
+  
+    for (let todo of todos) {
+      let isComplete = todo.isComplete
+  
+      if (isComplete === true) {
+        toDoBody.innerHTML += `
+      <ul data-testid="toDoItem" class="completed" data-todoId="${todo.id}" >${todo.text} 
+      <button data-testid="deleteButton" class="delete" onclick="deleteButton(event)" >Delete</button>
+      </ul>
+      `
+      } else if (isComplete === false) {
+        toDoBody.innerHTML += `
+        <ul data-testid="toDoItem" data-todoId="${todo.id}" >${todo.text} 
+        <button data-testid="completeButton" class="complete" onclick="makeComplete(event)" >Complete</button>
+        <button data-testid="deleteButton" class="delete" onclick="deleteButton(event)" >Delete</button>
+        </ul>
+        `
+      }
+    }
+  }
