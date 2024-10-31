de2dc333cbc65259cb95e0861ae556ff5c14923d
Jacob Larson <hockeybobber@comcast.net>
2024-10-30 21:31:18 -0500

    formatted doc

--- server/public/scripts/client.ts
+++ server/public/scripts/client.ts
@@ -1,42 +1,41 @@
-console.log('JS is sourced!');
-
+console.log("JS is sourced!");
 
 //get route
 function getTodos() {
-    console.log('in get route');
+  console.log("in get route");
 
   axios({
-    url: '/todos',
-    method: 'GET'
+    url: "/todos",
+    method: "GET",
   }).then((response) => {
-    renderTodos(response.data)
-  })
+    renderTodos(response.data);
+  });
 }
 
 //render route
 function renderTodos(todos) {
-    console.log('in render function');
-    const toDoBody: any = document.getElementById('todoBody');
-    toDoBody.innerHTML = '';
-  
-    for (let todo of todos) {
-      let isComplete = todo.isComplete
-  
-      if (isComplete === true) {
-        toDoBody.innerHTML += `
+  console.log("in render function");
+  const toDoBody: any = document.getElementById("todoBody");
+  toDoBody.innerHTML = "";
+
+  for (let todo of todos) {
+    let isComplete = todo.isComplete;
+
+    if (isComplete === true) {
+      toDoBody.innerHTML += `
       <ul class="completed" >${todo.text} 
       <button class="delete" onclick="deleteButton(event)" >Delete</button>
       </ul>
-      `
-      } else if (isComplete === false) {
-        toDoBody.innerHTML += `
+      `;
+    } else if (isComplete === false) {
+      toDoBody.innerHTML += `
         <ul>${todo.text} 
         <button class="complete" onclick="makeComplete(event)" >Complete</button>
         <button class="delete" onclick="deleteButton(event)" >Delete</button>
         </ul>
-        `
-      }
+        `;
     }
   }
+}
 
-  getTodos()
+getTodos();
