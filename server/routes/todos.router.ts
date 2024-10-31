500079088542c9c3552c8d953e3fcae8c8d24cf0
Jacob Larson <hockeybobber@comcast.net>
2024-10-30 21:24:11 -0500

    added get route to the router

--- server/routes/todos.router.ts
+++ server/routes/todos.router.ts
@@ -2,6 +2,26 @@ const router = require('express').Router();
 const pool = require('../modules/pool');
 
 
+//get route
+router.get('/', (req, res) => {
+    console.log('in get route');
+
+    let queryText = `
+    SELECT * FROM "todos" 
+    ORDER BY "id";
+    `
+  pool.query(queryText)
+    .then(result => {
+        console.log('result', result.rows);
+        // Sends back the results in an object
+        res.send(result.rows);
+  })
+  .catch(err => {
+    console.log('error getting todos', err);
+    res.sendStatus(500);
+  });
+})
+
 
 
 module.exports = router;
